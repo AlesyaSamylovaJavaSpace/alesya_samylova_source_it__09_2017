de696ae92751cacb42842b2e9b33a0c8f2e9733a
Alesya Samylova <alemari.samylovy44@gmail.com>
2017-10-08 17:39:14 +0300

    Home Work 6: Three tasks were completed. One last is not started

--- HomeWork6/demo/HomeWork6Demo.java
+++ HomeWork6/demo/HomeWork6Demo.java
@@ -6,24 +6,40 @@ import task1.util.ReviewMark;
 import task3.MyUtilityClass;
 
 import java.util.Arrays;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class HomeWork6Demo {
+
     public static void main(String[] args) {
         System.out.println("--- Demonstration of Home Work 6: working on Exceptions ---\n");
+        showSelectionMenu();
+    }
 
-
-        System.out.println("--- Tasks 2 and 3: Custom Exceptions. Handling exceptions inside utility class");
-        MyUtilityClass <String> myUtilityClassString = new MyUtilityClass<String>();
-        String[] myStringArray1 = {"Hello", "Hi", "Holla", "Bonjurno"};
-        String[] myStringArray2 = null;
-        Integer[] indexes = {2, 5, 3};
-        System.out.println("1) Get element by index = " + indexes[0] + " from array = " + Arrays.toString(myStringArray1));
-        System.out.println(" Element is " + myUtilityClassString.getElementByIndex(myStringArray1, indexes[0]));
-        System.out.println("2) Get element by index = " + indexes[1] + " from array = " + Arrays.toString(myStringArray1));
-        System.out.println(" Element is " + myUtilityClassString.getElementByIndex(myStringArray1, indexes[1]));
-        System.out.println("3) Get element by index = " + indexes[2] + " from array = " + Arrays.toString(myStringArray2));
-        System.out.println(" Element is " + myUtilityClassString.getElementByIndex(null, indexes[2]));
-
+    public static void showSelectionMenu(){
+        System.out.println("Select a number of task to be demonstrated. From 1 to 4: ");
+        Scanner myScanner = new Scanner(System.in);
+        try {
+            Integer myTaskNumber = myScanner.nextInt();
+            switch (myTaskNumber) {
+                case 1:
+                    getDemoOfTask1();
+                    break;
+                case 2:
+                /*FALLTHROUGH*/
+                case 3:
+                    getDemoOfTasks2And3();
+                    break;
+                case 4:
+                    break;
+                default:
+                    System.out.println("There is no task number " + myTaskNumber);
+            }
+        } catch (InputMismatchException ex){
+            System.out.println("You should have entered the integer value!!!");
+        } finally {
+            myScanner.close();
+        }
     }
 
     public static void getDemoOfTask1(){
@@ -43,6 +59,58 @@ public class HomeWork6Demo {
                 System.out.println(myTask1.getHelloMessage(null));
             } catch (Exception e) {
                 System.out.println("EXCEPTION: " + e.getMessage());
+            }
+        }
+    }
+
+    public static void getDemoOfTasks2And3(){
+        System.out.println("--- Tasks 2 and 3: Custom Exceptions. Handling exceptions inside utility class");
+        MyUtilityClass <String> myUtilityClassString = new MyUtilityClass<String>();
+        String[] myStringArray1 = {"Hello", "Hi", "Holla", "Bonjurno"};
+        String[] myStringArray2 = null;
+        Integer[] indexes = {2, 5, 3};
+        System.out.println("Testing of method 1: get element of array by its index");
+        System.out.println("1) Get element by index = " + indexes[0] + " from array = " + Arrays.toString(myStringArray1));
+        System.out.println(" Element is " + myUtilityClassString.getElementByIndex(myStringArray1, indexes[0]));
+        System.out.println("2) Get element by index = " + indexes[1] + " from array = " + Arrays.toString(myStringArray1));
+        System.out.println(" Element is " + myUtilityClassString.getElementByIndex(myStringArray1, indexes[1]));
+        System.out.println("3) Get element by index = " + indexes[2] + " from array = " + Arrays.toString(myStringArray2));
+        System.out.println(" Element is " + myUtilityClassString.getElementByIndex(null, indexes[2]));
+
+        System.out.println("\nTesting of method 2: get element of array by its index");
+        String myStr1 = "56 Str";
+        String myStr2 = "56.16D";
+        Integer myInt = myUtilityClassString.convertStringToInteger(myStr1);
+        System.out.println("String \"" + myStr1 + "\"" + ((myInt != null) ? " was ":" wasn't ") + "converted to Integer");
+
+        System.out.println("\nTesting of methods 3 and 4: throwing custom unchecked vs checked exceptions");
+        try {
+            myUtilityClassString.obtainMyUncheckedException();
+        } catch (RuntimeException ex) {
+            ex.printStackTrace();
+        } finally {
+            try {
+                myUtilityClassString.obtainMyCheckedException();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            } finally {
+                System.out.println("\nTesting of method 5: processing Illegal Resource Exception");
+                try {
+                    myUtilityClassString.processIllegalResourceException();
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                } finally {
+                    System.out.println("\nTesting of method 6: Convert form Object to Double");
+                    Object myObject = new Object();
+                    Double myDouble =myUtilityClassString.convertObjectToDouble(myObject);
+                    System.out.println("Object \"" + myObject.toString() + "\"" + ((myDouble != null) ? " was ":" wasn't ") + "converted to Double");
+
+                    myDouble =myUtilityClassString.convertObjectToDouble(myStr1);
+                    System.out.println("String \"" + myStr1 + "\"" + ((myDouble != null) ? " was ":" wasn't ") + "converted to Double");
+
+                    myDouble =myUtilityClassString.convertObjectToDouble(myStr2);
+                    System.out.println("String \"" + myStr2 + "\"" + ((myDouble != null) ? " was ":" wasn't ") + "converted to Double");
+                }
             }
         }
     }
--- HomeWork6/task3/MyUtilityClass.java
+++ HomeWork6/task3/MyUtilityClass.java
@@ -1,5 +1,9 @@
 package task3;
 
+import jdk.management.resource.ResourceRequestDeniedException;
+import task2.MyCheckedException;
+import task2.MyUncheckedException;
+
 public class MyUtilityClass <T>{
     public T getElementByIndex(T[] array, int index) {
         T element = null;
@@ -17,5 +21,53 @@ public class MyUtilityClass <T>{
         }finally {
             return element;
         }
+    }
+
+    public Integer convertStringToInteger(String str) {
+        Integer intVar = null;
+        try {
+            intVar = Integer.valueOf(str);
+        } catch (NumberFormatException ex) {
+            System.out.println(" EXCEPTION " + ex.getClass().getCanonicalName()
+                    + " your string \"" + str + "\" can't be converted to Integer");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            return intVar;
+        }
+    }
+
+    public void obtainMyUncheckedException(){
+        throw new MyUncheckedException();
+    }
+
+    public void obtainMyCheckedException() throws MyCheckedException{
+        throw new MyCheckedException();
+    }
+
+    public void processIllegalResourceException() throws MyCheckedException{
+         try {
+             throw new ResourceRequestDeniedException();
+         } catch (ResourceRequestDeniedException ex) {
+             ex.printStackTrace();
+         } finally {
+             throw new MyCheckedException();
+         }
+    }
+
+    public Double convertObjectToDouble(Object o) {
+        Double doubleVar = null;
+        String strVar = null;
+        try {
+            strVar = (String) o;
+            doubleVar = Double.valueOf(strVar);
+        } catch (ClassCastException ex) {
+            System.out.println(" EXCEPTION " + ex.getClass().getCanonicalName()
+                    + ": your object cannot be cast to String");
+        } catch (NumberFormatException ex) {
+            System.out.println(" EXCEPTION " + ex.getClass().getCanonicalName()
+                    + " your string \"" + strVar + "\" can't be converted to Double");
+        }
+        return doubleVar;
     }
 }
